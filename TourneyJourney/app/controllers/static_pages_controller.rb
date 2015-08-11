158de4610f26b390fe1bdfe9b0d37355bd4e8823
darrengtan <darrengtan@gmail.com>
2015-08-11 11:27:05 -0700

    rename project to TournaGen

--- TourneyJourney/app/controllers/static_pages_controller.rb
+++
@@ -1,6 +0,0 @@
-class StaticPagesController < ApplicationController
-  before_action :require_log_in!
-
-  def root
-  end
-end
--- TourneyJourney/config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  resources :users, only: [:new, :create]
-  resource :session, only: [:new, :create, :destroy]
-
-  root to: "static_pages#root"
-end
